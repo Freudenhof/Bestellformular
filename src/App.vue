3ae82435ea44f179c12ec67bb7c837a56026335e
JannisX11 <jannis4236@gmail.com>
2025-01-12 20:57:05 +0100

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Formular from './components/Formular.vue'
+</script>
+
+<template>
+	<header>
+		<a href="https://freudenhof.de">Freudenhof</a>
+	</header>
+	<Formular />
+</template>
+
+<style scoped>
+header {
+	height: 50px;
+	padding: 8px;
+}
+</style>
